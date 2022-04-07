7bb9071527776d614cecda4b01123244de876767
Michael Solomon <michicko81@gmail.com>
2022-04-07 02:11:33 +0100

    moved cartfooter

--- client/src/components/user_components/Cart/CartContent.js
+++ client/src/components/user_components/Cart/CartContent.js
@@ -1,5 +1,6 @@
 import styled from "styled-components";
 import CartItem from "./CartItem";
+import CartFooter from "./CartFooter";
 
 const CartContent = ({ cartItems }) => {
 	return (
@@ -9,6 +10,7 @@ const CartContent = ({ cartItems }) => {
 					return <CartItem item={item} key={i} />;
 				})}
 			</ul>
+			<CartFooter />
 		</Styles>
 	);
 };
@@ -16,12 +18,13 @@ const CartContent = ({ cartItems }) => {
 const Styles = styled.section`
 	width: 100%;
 
-  ul{
-    list-style: none;
+	ul {
+		list-style: none;
+		margin-bottom: 4rem;
 
-    &>*:not(:last-child){
-      margin-bottom: 2rem;
-    }
-  }
+		& > *:not(:last-child) {
+			margin-bottom: 2rem;
+		}
+	}
 `;
 export default CartContent;
